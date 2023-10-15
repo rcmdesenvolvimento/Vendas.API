136da8c558dcd029f9fdf9c860ff63046b4c0881
= <=>
2023-10-15 18:46:18 -0300

    Criação da Classe Pessoa e da Classe  DomainValidationException

---
+++ Vendas.API.Domain/Entities/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using Vendas.API.Domain.Validation;
+
+namespace Vendas.API.Domain.Entities;
+
+public sealed class Pessoa
+{
+    public int Id { get; private set; }
+    public string? Nome { get; private set; }
+    public string? Documento { get; private set; }
+    public string? Telefone { get; private set; }
+
+    public Pessoa(string nome, string documento, string telefone)
+    {
+        Validation(nome, documento, telefone);
+    }
+
+    //Edição do registro
+    public Pessoa(int id, string nome, string documento, string telefone)
+    {
+        DomainValidationException.When(id < 0, "O Id deve ser maior que zero");
+
+        Id = id;
+
+        Validation(nome, documento, telefone);
+    }
+
+    private void Validation(string nome, string documento, string telefone)
+    {
+        DomainValidationException.When(string.IsNullOrEmpty(nome), "O Nome deve ser informado!");
+        DomainValidationException.When(string.IsNullOrEmpty(documento), "O Documento deve ser informado!");
+        DomainValidationException.When(string.IsNullOrEmpty(telefone), "O Telefone deve ser informado!");
+
+        Nome = nome;
+        Documento = documento;
+        Telefone = telefone;
+    }
+
+}
---
+++ Vendas.API.Domain/Validation/DomainValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Vendas.API.Domain.Validation;
+
+public class DomainValidationException : Exception
+{
+    public DomainValidationException(string error) : base(error) { }
+
+    public static void When(bool hasError, string message)
+    {
+        if (hasError) throw new DomainValidationException(message);
+    }
+
+}
