445687ed166d8c9126bd90afb83598393b14d44b
= <=>
2023-10-15 19:07:44 -0300

    Criação da Classe Produto.

---
+++ Vendas.API.Domain/Entities/Produto.cs
@@ -0,0 +1,37 @@
+﻿using Vendas.API.Domain.Validation;
+
+namespace Vendas.API.Domain.Entities;
+
+public sealed class Produto
+{
+    public int Id { get; private set; }
+    public string? Nome { get; private set; }
+    public string? CodErp { get; set; }
+    public decimal Preco { get; private set; }
+
+    public Produto(string nome, string codErp, decimal preco)
+    {
+        Validation(nome, codErp, preco);
+    }
+
+    //Edição do registro
+    public Produto(int id, string nome, string codErp, decimal preco)
+    {
+        DomainValidationException.When(id < 0, "O Id deve ser maior que zero");
+
+        Id = id;
+
+        Validation(nome, codErp, preco);
+    }
+
+    private void Validation(string nome, string codErp, decimal preco)
+    {
+        DomainValidationException.When(string.IsNullOrEmpty(nome), "O Nome deve ser informado!");
+        DomainValidationException.When(string.IsNullOrEmpty(codErp), "O CodErp deve ser informado!");
+        DomainValidationException.When(preco < 0, "O Preço do produto deve ser informado!");
+
+        Nome = nome;
+        CodErp = codErp;
+        Preco = preco;
+    }
+}
